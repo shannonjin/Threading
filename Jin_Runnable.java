155a391212454f538ec72b04f3b226f35c2d64f7
shannonjin <shannonj112@gmail.com>
2016-10-27 21:49:50 -0400

    Add files via upload

---
+++ Jin_Runnable.java
@@ -0,0 +1,5 @@
+package threading;
+
+public class Jin_Runnable {
+
+}
---
+++ scratch.java
@@ -0,0 +1,16 @@
+package threading;
+
+public class scratch {
+	
+	static int a;
+	
+	public static void foo(){
+		
+		Object lock=new Object();
+		
+		synchronized(lock){
+			
+		}
+	}
+
+}
